221b57e4f75fd45aad7ee5f46a21befcb23e3d1c
Asaprof <50611219+Asaprof@users.noreply.github.com>
2020-06-04 17:44:58 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,45 @@
+
+    const Dashboard = httpVueLoader('src/weather.vue');
+    const Transactions = httpVueLoader('src/transactions.vue');
+
+
+    const router = new VueRouter({
+        routes: [
+            { path: '/dashboard', component: Dashboard},
+            { path: '/transactions', component: Transactions }
+          ]
+    })
+
+    const app = new Vue({
+      el: '#app',
+      router: router,
+      data: {
+          weather: axios.get('http://vhost115919.ispsite.ru/weather.json'),
+          date: new Date(),
+          options: {
+              year: 'numeric',
+              day: 'numeric',
+              month: 'long',
+              weekday: 'long',
+              hour: 'numeric',
+              minute: 'numeric'
+          },
+      }
+    })
+
+    const store = new Vuex.Store({
+        actions: {},
+        mutations: {
+            addTransaction(state, name) {
+                state.transactionsList.push(name)
+            }
+        },
+        state: {
+            transactionsList: [1,2,3]
+        },
+        getters: {},
+
+        modules: {}
+    })
+
+
---
+++ src/transactions.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="add">
+            
+        </div>
+        <div class="transactions">
+            <table width="100%">
+                <thead>
+                    <th>Transactions</th>
+                </thead>
+                <tbody class="transactions-data">
+                    <tr>
+                        <th>Sell 1</th>
+                    </tr>
+                    <tr>
+                        <th>Sell 2</th>
+                    </tr>
+                    <tr>
+                        <th>Sell 3</th>
+                    </tr>
+                    <tr>
+                        <th>Sell 4</th>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
